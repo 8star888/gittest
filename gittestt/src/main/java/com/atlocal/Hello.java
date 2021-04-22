6a1961a442b1a6f277173a92fb6e9cc42f3c499c
8star88 <stardream929@gmail.com>
2021-04-22 16:06:19 +0800

    version1

---
+++ gittestt/src/main/java/com/atlocal/Hello.java
@@ -0,0 +1,12 @@
+package com.atlocal;
+
+/**
+ * @author stardream  Email:stardream0000@163.com
+ * @Description
+ * @ 2021/4/22 15:51
+ */
+public class Hello {
+    public void print(String s){
+        System.out.println(s);
+    }
+}
